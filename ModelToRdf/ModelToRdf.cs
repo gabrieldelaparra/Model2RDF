5704a1f1c718ae359518f37393c1adf885785d2e
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-10-28 22:23:25 +0100

    Add SerializeNtriples; Change all formatters to use RDF11 syntax (UTF8 support)

--- ModelToRdf/ModelToRdf.cs
+++ ModelToRdf/ModelToRdf.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using ModelToRdf.Extensions;
 using Newtonsoft.Json.Linq;
 using VDS.RDF;
-using VDS.RDF.Query.Expressions.Functions.XPath.String;
+using VDS.RDF.Parsing;
 using VDS.RDF.Writing.Formatting;
+using Wororo.Utilities;
 
 namespace ModelToRdf
 {
@@ -14,16 +15,27 @@ namespace ModelToRdf
     {
         public static string DefaultIri { get; set; } = @"http://model2.rdf/";
 
-        public static IEnumerable<string> GraphToNTriples(this Graph graph) {
+        public static void SerializeNTriples(this Graph graph, string outputFilename)
+        {
+            var triples = graph.GraphToNTriples();
+            //var writer = new NTriplesWriter(NTriplesSyntax.Rdf11) { SortTriples = true};
+            outputFilename.CreatePathIfNotExists();
+            File.WriteAllLines(outputFilename, triples);
+            //writer.Save(graph, outputFilename);
+        }
+
+        public static IEnumerable<string> GraphToNTriples(this Graph graph)
+        {
             //Create a formatter
-            ITripleFormatter formatter = new NTriplesFormatter();
+            ITripleFormatter formatter = new NTriplesFormatter(NTriplesSyntax.Rdf11);
 
             //Print triples with this formatter
             foreach (var t in graph.Triples) yield return t.ToString(formatter);
         }
 
         //TODO: Should convert to a custom object (json/dictionary?), to avoid cyclic calls.
-        public static Graph ToRDFGraph(this IDictionary<string, JToken> jDictionary, string defaultIri = "") {
+        public static Graph ToRDFGraph(this IDictionary<string, JToken> jDictionary, string defaultIri = "")
+        {
             if (!string.IsNullOrWhiteSpace(defaultIri)) DefaultIri = defaultIri;
             var graph = new Graph();
             jDictionary.ToRDFGraph(graph);
@@ -31,13 +43,15 @@ namespace ModelToRdf
         }
 
         //TODO: This should be external. The user should be able to specify what is the Id.
-        internal static string GetId(this IDictionary<string, JToken> jDictionary) {
+        internal static string GetId(this IDictionary<string, JToken> jDictionary)
+        {
             var idPairs = jDictionary.Where(x => x.Key.ToLower() == "id" || x.Key.ToLower() == "@id");
             if (idPairs.Any()) return idPairs.FirstOrDefault().Value.ToString();
             return Guid.NewGuid().ToString();
         }
 
-        internal static void ToRDFGraph(this IDictionary<string, JToken> jDictionary, Graph graph) {
+        internal static void ToRDFGraph(this IDictionary<string, JToken> jDictionary, Graph graph)
+        {
             if (jDictionary is null) return;
 
             var id = jDictionary.GetId();
@@ -47,20 +61,25 @@ namespace ModelToRdf
             jDictionary.ToRDFGraph(graph, entityNode);
         }
 
-        internal static void ToRDFGraph(this JToken jToken, string key, IUriNode entityNode, Graph graph) {
+        internal static void ToRDFGraph(this JToken jToken, string key, IUriNode entityNode, Graph graph)
+        {
             //TODO: This is very specific to E3 (==0)
             //if (string.IsNullOrWhiteSpace(jToken.ToString()) || jToken.ToString().Equals("0"))
             //    return;
             //TODO: Implement: If has attribute "Id";
-            if (key.ToLower().Equals("id") || key.ToLower().Equals("@id")) {
+            if (key.ToLower().Equals("id") || key.ToLower().Equals("@id"))
+            {
                 graph.Assert(entityNode, key.ToUriNode(DefaultIri), jToken.ToString().ToLiteralNode());
             }
-            else if (key.ToLower().EndsWith("id") || key.ToLower().EndsWith("ids")) {
+            else if (key.ToLower().EndsWith("id") || key.ToLower().EndsWith("ids"))
+            {
                 graph.Assert(entityNode, key.ToUriNode(DefaultIri), jToken.ToString().ToUriNode(DefaultIri));
             }
-            else {
+            else
+            {
                 //TODO: Create a node for each type:
-                switch (jToken.Type) {
+                switch (jToken.Type)
+                {
                     case JTokenType.Date:
                         var dateTime = DateTime.Parse(jToken.ToString());
                         graph.Assert(entityNode, key.ToUriNode(DefaultIri), dateTime.ToString("yyyy-MM-dd HH:mm:ss").ToLiteralNode());
@@ -86,31 +105,36 @@ namespace ModelToRdf
                         graph.Assert(entityNode, key.ToUriNode(DefaultIri), jToken.ToString().ToLiteralNode());
                         break;
                 }
-                
+
             }
         }
 
         //TODO: This should be external. The user should be able to specify which properties to map and how.
         //TODO: Add ClassName as a Predicate, with the 'model.GetType().Name'
-        internal static void ToRDFGraph(this IDictionary<string, JToken> model, Graph graph, IUriNode entityNode) {
+        internal static void ToRDFGraph(this IDictionary<string, JToken> model, Graph graph, IUriNode entityNode)
+        {
             if (model is null) return;
 
             var keys = model.Keys;
 
-            foreach (var key in keys) {
+            foreach (var key in keys)
+            {
                 var value = model[key];
                 if (value == null) return;
 
-                if (value.Type.ToString().Equals("Array")) {
+                if (value.Type.ToString().Equals("Array"))
+                {
                     if (value is JArray jArray)
                         foreach (var item in jArray)
-                            if (item is IDictionary<string, JToken> itemDict) {
+                            if (item is IDictionary<string, JToken> itemDict)
+                            {
                                 //TODO: Add reference to this object:
                                 var id = itemDict.GetId().ToUriNode(DefaultIri);
                                 graph.Assert(entityNode, key.ToUriNode(DefaultIri), id);
                                 itemDict.ToRDFGraph(graph, id);
                             }
-                            else {
+                            else
+                            {
                                 if (item is JToken itemToken)
                                     itemToken.ToRDFGraph(key, entityNode, graph);
                                 else
@@ -119,17 +143,21 @@ namespace ModelToRdf
                     else
                         throw new Exception($"Unhandled scenario: Key: {key}; Data: {value}");
                 }
-                else if (value.Type.ToString().Equals("Object")) {
-                    if (value is IDictionary<string, JToken> itemDict) {
+                else if (value.Type.ToString().Equals("Object"))
+                {
+                    if (value is IDictionary<string, JToken> itemDict)
+                    {
                         var id = Guid.NewGuid().ToString().ToUriNode(DefaultIri);
                         graph.Assert(entityNode, key.ToUriNode(DefaultIri), id);
                         itemDict.ToRDFGraph(graph, id);
                     }
-                    else {
+                    else
+                    {
                         throw new Exception($"Unhandled scenario: Key: {key}; Data: {value}");
                     }
                 }
-                else {
+                else
+                {
                     value.ToRDFGraph(key, entityNode, graph);
                 }
             }
